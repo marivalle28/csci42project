e2ed3e96ea5fce30f6eac91c551263e309d3e381
Andrea Pilapil <andrea.pilapil@obf.ateneo.edu>
2022-03-15 18:43:56 +0800

    Update urls.py

--- apps/urls.py
+++ apps/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('home/', views.home, name='home'),
     path('takequiz/', views.takequiz, name='takequiz'),
     path('findresto/', views.findresto, name='findresto'),
