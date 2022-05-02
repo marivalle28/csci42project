8de2577d7715fb347a572b25ade3986a1ad17018
leanneena <101540875+leanneena@users.noreply.github.com>
2022-05-02 20:59:27 +0800

    Added Search Results for BGC Location

--- apps/urls.py
+++ apps/urls.py
@@ -7,4 +7,15 @@ urlpatterns = [
     path('takequiz/', views.takequiz, name='takequiz'),
     path('findresto/', views.findresto, name='findresto'),
     path('quizresults/', views.quizresults, name='quizresults'),
+    path('bgc/', views.bgc, name='bgc'),
+
+    path('dintaifung/', views.dintaifung, name='dintaifung'),
+    path('lapicara/', views.lapicara, name='lapicara'),
+    path('mcdo/', views.mcdo, name='mcdo'),
+    path('mendokoro/', views.mendokoro, name='mendokoro'),
+    path('shakeshack/', views.shakeshack, name='shakeshack'),
+    path('singleorigin/', views.singleorigin, name='singleorigin'),
+    path('thealley/', views.thealley, name='thealley'),
+    path('wangfu/', views.wangfu, name='wangfu'),
+
 ]
--- apps/views.py
+++ apps/views.py
@@ -11,3 +11,31 @@ def findresto(request):
 
 def quizresults(request):
 		return render(request, "apps/quizresults.html")
+
+def bgc(request):
+		return render(request, "apps/bgc.html")
+
+# resto views
+def dintaifung(request):
+		return render(request, "apps/resto_dintaifung.html")
+
+def lapicara(request):
+		return render(request, "apps/resto_lapicara.html")
+
+def mcdo(request):
+		return render(request, "apps/resto_mcdo.html")
+
+def mendokoro(request):
+		return render(request, "apps/resto_mendokoro.html")
+
+def shakeshack(request):
+		return render(request, "apps/resto_shakeshack.html")
+
+def singleorigin(request):
+		return render(request, "apps/resto_singleorigin.html")
+
+def thealley(request):
+		return render(request, "apps/resto_thealley.html")
+
+def wangfu(request):
+		return render(request, "apps/resto_wangfu.html")
